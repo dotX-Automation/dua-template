7ab0a0973d08dd99de7303e6b9133c0c0826e5bb
Roberto Masocco <robmasocco@gmail.com>
2023-04-04 17:14:56 +0200

    feat: filesystem layout

---
+++ docker/context/aliases.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Custom aliases for container internal shell.
+#
+# Roberto Masocco <robmasocco@gmail.com>
+# Intelligent Systems Lab <isl.torvergata@gmail.com>
+#
+# April 4, 2023
+
+# Add custom, general-purpose aliases here.
+
+alias ls='ls --color=auto'
+alias ll='ls -la'
+alias valgrind-check='valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose --log-file=valgrind.out'
