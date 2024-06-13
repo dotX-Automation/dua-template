f591b4b0c368465912efe9dfbac991fcc0251c03
Roberto Masocco <robmasocco@gmail.com>
2024-06-13 19:58:28 +0200

    fix: bashrc bugs

--- bin/dua-templates/context/aliases.sh
+++ bin/dua-templates/context/aliases.sh
@@ -2,10 +2,23 @@
 
 # Custom aliases for container internal shell.
 #
-# Roberto Masocco <robmasocco@gmail.com>
-# Intelligent Systems Lab <isl.torvergata@gmail.com>
+# Roberto Masocco <r.masocco@dotxautomation.com>
 #
-# April 4, 2023
+# June 13, 2024
+
+# Copyright 2024 dotX Automation s.r.l.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 # Add custom, general-purpose aliases here.
 # You can also source other files from sub-units included by this project.
--- bin/dua-templates/context/ros2.sh
+++ bin/dua-templates/context/ros2.sh
@@ -2,14 +2,27 @@
 
 # Shell functions and commands for ROS 2 management.
 #
-# Roberto Masocco <robmasocco@gmail.com>
-# Intelligent Systems Lab <isl.torvergata@gmail.com>
+# Roberto Masocco <r.masocco@dotxautomation.com>
 #
-# April 4, 2023
+# June 13, 2024
 
-# shellcheck disable=SC1090
+# Copyright 2024 dotX Automation s.r.l.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-# Initialize ROS 2 environment according to the Zsh shell.
+# shellcheck disable=SC1090,SC2086
+
+# Initialize ROS 2 environment according to the current shell.
 ros2init() {
   if [[ $# -ne 0 ]]; then
     export ROS_DOMAIN_ID=$1
@@ -18,40 +31,42 @@ ros2init() {
   export ROS_PYTHON_VERSION=3
   export ROS_DISTRO=humble
 
+  CURR_SHELL=$(ps -p $$ | awk 'NR==2 {print $4}')
+
   # Check that the ROS 2 installation is present, and source it
-  if [[ -f /opt/ros/$ROS_DISTRO/setup.zsh ]]; then
-    source /opt/ros/$ROS_DISTRO/setup.zsh
-  elif [[ -f /opt/ros/$ROS_DISTRO/install/setup.zsh ]]; then
-    source /opt/ros/$ROS_DISTRO/install/setup.zsh
+  if [[ -f /opt/ros/$ROS_DISTRO/setup.$CURR_SHELL ]]; then
+    source /opt/ros/$ROS_DISTRO/setup.$CURR_SHELL
+  elif [[ -f /opt/ros/$ROS_DISTRO/install/setup.$CURR_SHELL ]]; then
+    source /opt/ros/$ROS_DISTRO/install/setup.$CURR_SHELL
   else
     echo >&2 "ROS 2 installation not found."
     return 1
   fi
 
   # Source additional stuff for colcon argcomplete
-  source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.zsh
+  source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.$CURR_SHELL
 
   # Source Ignition Gazebo stuff
-  if [[ -f /opt/gazebo/fortress/install/setup.zsh ]]; then
-    source /opt/gazebo/fortress/install/setup.zsh
+  if [[ -f /opt/gazebo/fortress/install/setup.$CURR_SHELL ]]; then
+    source /opt/gazebo/fortress/install/setup.$CURR_SHELL
   fi
-  if [[ -f /opt/ros/ros_gz/install/local_setup.zsh ]]; then
-    source /opt/ros/ros_gz/install/local_setup.zsh
+  if [[ -f /opt/ros/ros_gz/install/local_setup.$CURR_SHELL ]]; then
+    source /opt/ros/ros_gz/install/local_setup.$CURR_SHELL
   fi
 
   # Source our fork of rmw_fastrtps
-  if [[ -f /opt/ros/rmw_fastrtps/install/local_setup.zsh ]]; then
-    source /opt/ros/rmw_fastrtps/install/local_setup.zsh
+  if [[ -f /opt/ros/rmw_fastrtps/install/local_setup.$CURR_SHELL ]]; then
+    source /opt/ros/rmw_fastrtps/install/local_setup.$CURR_SHELL
   fi
 
   # Source additional DUA stuff
-  if [[ -f /opt/ros/dua-utils/install/local_setup.zsh ]]; then
-    source /opt/ros/dua-utils/install/local_setup.zsh
+  if [[ -f /opt/ros/dua-utils/install/local_setup.$CURR_SHELL ]]; then
+    source /opt/ros/dua-utils/install/local_setup.$CURR_SHELL
   fi
 
   # Source workspace if present
-  if [[ -f /home/neo/workspace/install/local_setup.zsh ]]; then
-    source /home/neo/workspace/install/local_setup.zsh
+  if [[ -f /home/neo/workspace/install/local_setup.$CURR_SHELL ]]; then
+    source /home/neo/workspace/install/local_setup.$CURR_SHELL
   fi
 }
 
